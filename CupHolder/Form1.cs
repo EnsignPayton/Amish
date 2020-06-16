6c613c79dd4bf3b10b886f2c447b9512a098398c
Logan Payton <Logan.Payton@javs.com>
2020-06-16 12:12:09 -0400

    :poop: 2

---
+++ CupHolder/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CupHolder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void YesButton_Click(object sender, EventArgs e)
+        {
+            mciSendString("set cdaudio door open", null, 0, IntPtr.Zero);
+            Close();
+        }
+
+        private void NoButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", CharSet = CharSet.Ansi)]
+        private static extern int mciSendString(string lpstrCommand, StringBuilder lpstrReturnString, int uReturnLength, IntPtr hwndCallback);
+    }
+}
